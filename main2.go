bba56cde00adc700147a9edd326440bd9e58c280
rassx6 <rass06@mail.ru>
2023-12-05 14:00:06 +0600

    a2

--- main2.go
+++ main2.go
@@ -2,29 +2,48 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
-	http.ListenAndServe(":80", nil)
-}
-func TestGETPlayers(t *testing.T) {
-	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
-		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 
-		got := response.Body.String()
-		want := "20"
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
 		}
-	})
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
